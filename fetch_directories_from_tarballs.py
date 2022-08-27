f64471043907500d83fa705ae41f34a73e5e623a
Ahmad, Mumtaz <Mumtaz_Ahmad@mentor.com>
2022-08-27 09:47:08 +0500

    fixed

--- fetch_directories_from_tarballs.py
+++ fetch_directories_from_tarballs.py
@@ -3,16 +3,37 @@ import os
 import subprocess
 import sys
 
-def fetch_aws_file(bucket,file_path):
-    cmd_str="aws s3 cp s3://"+bucket+file_path+" ."
-    output = subprocess.check_output(cmd_str, shell=True)
+def fetch_aws_file(bucket,filepath,download_folder):
+    filename=os.path.basename(filepath)
+    if os.path.exists(download_folder+"/"+filename):
+        print("Using cached copy of "+filename)
+    else:
+        print("Donloading "+filename)
+        cmd="aws s3 cp s3://"+bucket+filepath+" "+download_folder+"/"
+        subprocess.check_output(cmd, shell=True)
+    return download_folder+"/"+filename
 
 bucket="filesend.eps.mentorcloudservices.com"
+download_folder="downloads"
 tarballs_toprocess=[
-"/INDLIN/releases/industrial-os-2.4.1/oss/base-runtime.tar.gz",
-"/INDLIN/releases/industrial-os-2.4.1/oss/ipc-runtime.tar.gz"]
+"/INDLIN/releases/industrial-os-2.4.1/oss/siemens-runtime.tar.gz",
+"/INDLIN/releases/industrial-os-2.4.1/oss/ipc-runtime.tar.gz",
+"/INDLIN/releases/industrial-os-2.4.1/oss/base-runtime.tar.gz"]
+downloaded_files=[]
+os.system('mkdir -p scratch')
+try:
+  print("Fetching Tarballs from aws")
+  for tarball in tarballs_toprocess:
+      downloaded_files.append(fetch_aws_file(bucket,tarball,download_folder))
+except Exception as e:
+   print(e)
 
-print("Fetching Tarballs from aws")
-for tarball in tarballs_toprocess:
-    fetch_aws_file(bucket,tarball)
+try:
+  for tarball in downloaded_files:
+     print("Processing "+tarball)
+     cmd='tar -xvf '+tarball+' -C scratch'
+     subprocess.check_output(cmd, shell=True)
+except Exception as e:
+   print(e)
 
+print("Done")
