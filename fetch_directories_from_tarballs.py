6e8d82957dfeb49e4b528fdd9c5715f90fc724e5
Ahmad, Mumtaz <Mumtaz_Ahmad@mentor.com>
2022-08-27 08:49:37 +0500

    base

--- fetch_directories_from_tarballs.py
+++ fetch_directories_from_tarballs.py
@@ -1,6 +1,16 @@
-#! /usr/bin/python3
+#! /usr/bin/python
 import os
 import subprocess
 import sys
+
+def fetch_aws_file(file_path):
+    cmd_str="aws s3 cp "+file_path
+    output = subprocess.check_output(cmd_str, shell=True)
+
+bucket="filesend.eps.mentorcloudservices.com"
+tarballs_toprocess=[
+"/INDLIN/releases/industrial-os-2.4.1/oss/base-runtime.tar.gz",
+"/INDLIN/releases/industrial-os-2.4.1/oss/ipc-runtime.tar.gz"]
+
 
 print("Hello world")
