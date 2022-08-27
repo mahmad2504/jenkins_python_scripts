b5e72e09162cf5eb2c06e0e8ec4a0d69cbfb77b9
Ahmad, Mumtaz <Mumtaz_Ahmad@mentor.com>
2022-08-27 08:52:00 +0500

    fixed

--- fetch_directories_from_tarballs.py
+++ fetch_directories_from_tarballs.py
@@ -3,8 +3,8 @@ import os
 import subprocess
 import sys
 
-def fetch_aws_file(file_path):
-    cmd_str="aws s3 cp "+file_path
+def fetch_aws_file(bucket,file_path):
+    cmd_str="aws s3 cp s3://"+bucket+file_path
     output = subprocess.check_output(cmd_str, shell=True)
 
 bucket="filesend.eps.mentorcloudservices.com"
@@ -12,5 +12,7 @@ tarballs_toprocess=[
 "/INDLIN/releases/industrial-os-2.4.1/oss/base-runtime.tar.gz",
 "/INDLIN/releases/industrial-os-2.4.1/oss/ipc-runtime.tar.gz"]
 
+print("Fetching Tarballs from aws")
+for tarball in tarballs_toprocess:
+    fetch_aws_file(bucket,tarball)
 
-print("Hello world")
