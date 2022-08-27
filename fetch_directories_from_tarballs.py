837a5013dc34172ea84d5d70e967bd282e28632a
Ahmad, Mumtaz <Mumtaz_Ahmad@mentor.com>
2022-08-27 17:30:51 +0500

    updated

--- fetch_directories_from_tarballs.py
+++ fetch_directories_from_tarballs.py
@@ -14,14 +14,17 @@ tarballs_toprocess=[
 AWS_BUCKET=os.getenv('AWS_BUCKET') #"filesend.eps.mentorcloudservices.com"
 TARBALLS=os.getenv('TARBALLS') #"/INDLIN/releases/industrial-os-2.4.1/oss/siemens-runtime.tar.gz,/INDLIN/releases/industrial-os-2.4.1/oss/ipc-runtime.tar.gz"
 REBUILD=os.getenv('REBUILD') # "yes" pr "no"
-
-
+FOLDERS=os.getenv('FOLDERS') # comma delimited top level folder names 
+BUILD_ID=os.getenv('BUILD_ID')  
+OUTPUT=os.getenv('OUTPUT') 
 
 if AWS_BUCKET==None:
     print('AWS_BUCKET environmental variable not set')
+    exit(-1)
 
 if TARBALLS==None:
     print('TARBALLS environmental variable not set')
+    exit(-1)
 
 if REBUILD==None:
     REBUILD="no"
@@ -29,13 +32,29 @@ if REBUILD==None:
 if REBUILD=="yes":
     os.system('rm -rf scratch')
     os.system('rm -rf downloads')
+else:
+    if os.path.exists("scratch"):
+        print("Scratch folder exists, remove it and run again")
+        exit(-1)
 
-print("Parameter")
+if FOLDERS==None:
+    print('FOLDERS environmental variable not set')
+    exit(-1)
+
+
+if OUTPUT==None:
+    OUTPUT="output.tar.gz"
+  
+print("********* Parameters ***********")
+print("BUILD_ID="+BUILD_ID)
 print("AWS_BUCKET="+AWS_BUCKET)
 print("REBUILD="+REBUILD)
 print("TARBALLS="+TARBALLS)
-
+print("FOLDERS"+FOLDERS)
+print("OUTPUT="+OUTPUT)
+print("********************************")
 TARBALLS=TARBALLS.split(',')
+FOLDERS=FOLDERS.split(',')
     
 ##############################
 #Function to pull a file from aws bucket . aws should be configured with appropriate key on the node 
@@ -44,7 +63,7 @@ def fetch_aws_file(bucket,filepath,download_folder):
     if os.path.exists(download_folder+"/"+filename):
         print("Using cached copy of "+filename)
     else:
-        print("Donloading "+filename)
+        print("Downloading "+filename)
         cmd="aws s3 cp s3://"+bucket+filepath+" "+download_folder+"/"
         subprocess.check_output(cmd, shell=True)
     return download_folder+"/"+filename
@@ -52,25 +71,34 @@ def fetch_aws_file(bucket,filepath,download_folder):
 
 downloaded_files=[]
 os.system('mkdir -p scratch')
+os.system('mkdir -p downloads')
 try:
-  print("Fetching Tarballs from aws")
-  for tarball in tarballs_toprocess:
-      downloaded_files.append(fetch_aws_file(bucket=AWS_BUCKET,filepath=tarball,download_folder="downloads"))
+    print("Fetching Tarballs from aws")
+    for tarball in tarballs_toprocess:
+        downloaded_files.append(fetch_aws_file(bucket=AWS_BUCKET,filepath=tarball,download_folder="downloads"))
+except Exception as e:
+    os.system('rm -rf downloads')
+    print(e)
+    exit(-1)
+    
+try:
+    for tarball in downloaded_files:
+        print("Processing "+tarball)
+        cmd='tar -xvf '+tarball+' -C scratch'
+        subprocess.check_output(cmd, shell=True)
+except Exception as e:
+    os.system('rm -rf scratch')
+    print(e)
+    exit(-1)
+
+try:
+    cmd="tar -czf "+OUTPUT
+    for folder in FOLDERS:
+        cmd += " scratch/"+folder
+    subprocess.check_output(cstr, shell=True)
+    print("Done")
+    exit()
 except Exception as e:
    print(e)
-
-if os.path.exists("scratch"):
-   print("Scratch folder exists, remove it and run again")
    exit(-1)
-   
-try:
-  for tarball in downloaded_files:
-     print("Processing "+tarball)
-     cmd='tar -xvf '+tarball+' -C scratch'
-     subprocess.check_output(cmd, shell=True)
-except Exception as e:
-   print(e)
-
-print("Done")
-
-"/INDLIN/releases/industrial-os-2.4.1/oss/siemens-runtime.tar.gz","/INDLIN/releases/industrial-os-2.4.1/oss/ipc-runtime.tar.gz","/INDLIN/releases/industrial-os-2.4.1/oss/base-runtime.tar.gz"
+=
