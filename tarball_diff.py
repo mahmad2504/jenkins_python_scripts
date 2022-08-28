88879d489346b937ea90531e039d7b7d4a1dd451
Ahmad, Mumtaz <Mumtaz_Ahmad@mentor.com>
2022-08-28 23:29:10 +0500

    updated

--- tarball_diff.py
+++ tarball_diff.py
@@ -96,7 +96,7 @@ except Exception as e:
 
 print("Generating packages list for v1")
 
-if !os.path.exists(c):
+if !os.path.exists(packages1_list_file):
     with open(packages1_list_file, 'w') as pfile:
         for f in downloaded_filesv1:
             tar=tarfile.open(f)
