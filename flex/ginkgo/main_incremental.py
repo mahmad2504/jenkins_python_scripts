6d8439fc6fdaafafe840b7b035e82584098e0a10
Ahmad, Mumtaz <Mumtaz_Ahmad@mentor.com>
2022-09-03 14:59:31 +0500

    updated

--- flex/ginkgo/main_incremental.py
+++ flex/ginkgo/main_incremental.py
@@ -7,6 +7,6 @@ import tarfile
 from common import *
 
 def main_incremental(params):
-    cmd='tar -xjf '+params.build_folder+"/repotop.tar.bz2"
+    cmd='tar -xjf '+params.build_path+"/repotop.tar.bz2"
     sh(cmd)
     pass
