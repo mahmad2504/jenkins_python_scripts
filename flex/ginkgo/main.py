93f337ea7d9bf98f6fde4bc0bc6cefb6848268c4
Ahmad, Mumtaz <Mumtaz_Ahmad@mentor.com>
2022-09-03 10:41:11 +0500

    updated

--- flex/ginkgo/main.py
+++ flex/ginkgo/main.py
@@ -6,6 +6,7 @@ import hashlib
 sys.path.append('./scripts/flex/ginkgo')
 from checkout import *
 from common import *
+import yaml
 
 class ginkgo:
     params={'repo_url':"ssh://git@github.com:22/MentorEmbedded/mel-manifest.git",'repo_branch':"master"}
@@ -13,6 +14,8 @@ class ginkgo:
         self.params['WORKSPACE']=os.getenv('WORKSPACE')
         self.params['BUILD_NUMBER']=os.getenv('BUILD_NUMBER')
     def checkout(self):
-        checkout(DictObj(self.params));
+        params=DictObj(self.params)
+        print(yaml.dump(params))
+        checkout(DictObj(params));
     
     
