ec785131ca2fe297945a6c8eb50ada5cb9a05c71
Ahmad, Mumtaz <Mumtaz_Ahmad@mentor.com>
2022-09-03 09:33:31 +0500

    updated

--- common.py
+++ common.py
@@ -35,10 +35,8 @@ def sh(command):
        raise ProcessException(command, exitCode, output)
        
 def checkdiskspace(dspath,dskerrlimit):
-    cmd="df -P "+dspath+" | sed '1d' | awk '{print $4}' | tr -d '\n'"
-    print(cmd)
-    return 0
-    #dskspc=subprocess.check_output(cmd)
+    cmd="df -P "+dspath+" | sed '1d' | awk '{print $4}' | tr -d '\\n'"
+    dskspc=subprocess.check_output(cmd)
    
     print(dskspc)
     print(dskerrlimit)
